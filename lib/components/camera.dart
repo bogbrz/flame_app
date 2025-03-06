04008246354e1992525ffc48507df83600f82409
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2025-03-06 11:18:21 +0100

    implement rendering

---
+++ lib/components/camera.dart
@@ -0,0 +1,7 @@
+import 'package:flame/components.dart';
+
+class Camera extends PositionComponent {
+  @override
+  bool get debugMode => true;
+  
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'dart:math';
-import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame_app/klondike_game.dart';
+import 'package:flutter/material.dart';
 
 
 void main() {
+   final game = KlondikeGame();
   runApp(GameWidget(
-      game: FlameGame(),
+      game: game,
     ),);
 }
 
